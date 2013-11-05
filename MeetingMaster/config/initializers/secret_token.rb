4c4864e46ac291d54be2adad0fe724dae1013290
pedro.barata <pedro.barata@feedzai.com>
2013-11-05 23:58:35 +0000

    teste

---
+++ MeetingMaster/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeetingMaster::Application.config.secret_key_base = 'b825115984a2b86496cc378f5ae2953bf73926b70825f6d9a71ca7aa831fec95ebabdbff22cb23aa08296ed166005c2b71320d16f014ea5b8a56bef728bdf25f'
---
+++ MeetingMaster/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MeetingMaster::Application.config.session_store :cookie_store, key: '_MeetingMaster_session'
